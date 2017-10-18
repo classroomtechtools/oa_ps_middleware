380fcfc12b4ea63ecd6b98f7e5323a5d880bb774
Adam Morris <adammorris@MsPaulineDavidsonlaptop.local>
2017-10-18 11:03:34 +0800

    only print IDs

--- download.py
+++ download.py
@@ -11,22 +11,26 @@ import requests
 
 APIKEY = '119fa52af433c6304a44f18e866150e9'
 payload = {
-	'auth_token': APIKEY,
-	'since_id': 0
+    'auth_token': APIKEY,
+    'since_id': 0
 }
 url = 'https://igbis.openapply.com/api/v1/students'
 app_url = 'powerschool.com'
-columns = None
+properties = None
 
 links = []
-response = requests.get(url, params=payload)
-json = response.json()
 
 while True:
-	payload['since_id'] = json['students'][-1]['id']
-	for student in json['students']:
-		if columns is None:
-			columns = sorted(student.keys())
-		print("\n{id}: {name}".format(id=student.get('id'), name=(student.get('first_name') + ' ' + student.get('last_name'))).upper())
-		for column in columns:
-			print(' ' * 6, column, ':', student[column])
+
+    response = requests.get(url, params=payload)
+    data = response.json()
+
+    for student in data['students']:
+        if properties is None:
+            properties = sorted(student.keys())
+        print(student.get('id'))
+        #print("\n{id}: {name}".format(id=student.get('id'), name=(student.get('first_name') + ' ' + student.get('last_name'))).upper())
+        # for prop in properties:
+        #   print(' ' * 6, prop, ':', student[prop])
+    
+    payload['since_id'] = data['students'][-1]['id']            
